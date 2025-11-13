afe79a21d50aeebd7c90643bcd2fd89975205ba4
OLEG BASHYLOV <olegbashylov@gmail.com>
2025-11-13 14:03:40 +0200

    team-section_FULLDone

--- src/components/layout/slider/slider.js
+++ src/components/layout/slider/slider.js
@@ -34,8 +34,8 @@ function initSliders() {
 			modules: [Navigation],
 			observer: true,
 			observeParents: true,
-			slidesPerView: 1,
-			spaceBetween: 0,
+			//slidesPerView: 3,
+			spaceBetween: 32,
 			//autoHeight: true,
 			speed: 800,
 
@@ -72,31 +72,20 @@ function initSliders() {
 
 			// Кнопки "вліво/вправо"
 			navigation: {
-				prevEl: '.swiper-button-prev',
-				nextEl: '.swiper-button-next',
+				prevEl: '.slider__btn-prev',
+				nextEl: '.slider__btn-next',
 			},
-			/*
+
 			// Брейкпоінти
 			breakpoints: {
 				640: {
-					slidesPerView: 1,
-					spaceBetween: 0,
-					autoHeight: true,
-				},
-				768: {
 					slidesPerView: 2,
-					spaceBetween: 20,
 				},
 				992: {
 					slidesPerView: 3,
-					spaceBetween: 20,
-				},
-				1268: {
-					slidesPerView: 4,
-					spaceBetween: 30,
 				},
 			},
-			*/
+
 			// Події
 			on: {
 
